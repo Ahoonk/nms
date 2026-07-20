031e2ab34d535644bcfa7e7eb16fed75a578cda3
Ahoonk <ncyahonk@gmail.com>
2026-07-20 19:03:02 +0700

    add url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,7 @@ use App\Services\Zabbix\ZabbixApiClient;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
