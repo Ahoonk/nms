44d26750a41773d185a8f6db2a397db6367725aa
Ahoonk <ncyahonk@gmail.com>
2026-07-20 19:01:31 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -38,6 +38,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+    if ($this->app->environment('production')) {
+        URL::forceScheme('https');
+    }
+
         Vite::prefetch(concurrency: 3);
 
         Gate::before(function (User $user) {
