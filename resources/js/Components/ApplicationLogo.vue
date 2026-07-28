bbbb68adc4139eb4318327a5e4f10de1c8c96738
Ahoonk <ncyahonk@gmail.com>
2026-07-28 15:44:23 +0700

    ganti logo

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,55 +1,16 @@
+<script setup>
+const props = defineProps({
+    src: {
+        type: String,
+        default: '/images/aska-logo.png',
+    },
+    alt: {
+        type: String,
+        default: 'ASKA logo',
+    },
+});
+</script>
+
 <template>
-    <svg
-        viewBox="0 0 640 760"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-label="ASKA logo"
-        role="img"
-        preserveAspectRatio="xMidYMid meet"
-    >
-        <rect width="640" height="760" fill="none" />
-        <path
-            d="M320 54 L430 214 L398 214 L320 96 L242 214 L210 214 Z"
-            fill="#e31c24"
-        />
-        <path
-            d="M320 135 L448 356 L410 356 L320 200 L230 356 L192 356 Z"
-            fill="none"
-            stroke="#1f2ea8"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="22"
-        />
-        <path
-            d="M320 235 L472 492 L434 492 L320 290 L206 492 L168 492 Z"
-            fill="none"
-            stroke="#1f2ea8"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="20"
-        />
-        <path
-            d="M166 538 L216 438 L248 438 L200 536 L200 648 L276 648 L320 520 L364 648 L440 648 L440 536 L392 438 L424 438 L474 538"
-            fill="none"
-            stroke="#1f2ea8"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="24"
-        />
-        <text
-            x="320"
-            y="720"
-            text-anchor="middle"
-            fill="#e31c24"
-            font-family="Arial Black, Arial, Helvetica, sans-serif"
-            font-size="112"
-            font-weight="900"
-            letter-spacing="6"
-            style="paint-order: stroke fill"
-            stroke="#1f2ea8"
-            stroke-width="12"
-            stroke-linejoin="round"
-        >
-            ASKA
-        </text>
-    </svg>
+    <img :src="props.src" :alt="props.alt" class="block h-auto w-auto" />
 </template>
