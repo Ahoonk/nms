15db63258558c47a35a5ebe213f580c448e12a52
Ahoonk <ncyahonk@gmail.com>
2026-07-28 15:39:36 +0700

    ganti layout logo

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,39 +1,52 @@
 <template>
     <svg
-        viewBox="0 0 640 720"
+        viewBox="0 0 640 760"
         xmlns="http://www.w3.org/2000/svg"
         aria-label="ASKA logo"
         role="img"
+        preserveAspectRatio="xMidYMid meet"
     >
-        <rect width="640" height="720" fill="none" />
+        <rect width="640" height="760" fill="none" />
         <path
-            d="M320 44 L420 214 L398 214 L320 84 L242 214 L220 214 Z"
+            d="M320 54 L430 214 L398 214 L320 96 L242 214 L210 214 Z"
             fill="#e31c24"
         />
         <path
-            d="M172 454 L140 492 L140 660 L230 660 L320 460 L410 660 L500 660 L500 492 L468 454 L468 628 L414 628 L320 426 L226 628 L172 628 Z"
-            fill="#1f2ea8"
+            d="M320 135 L448 356 L410 356 L320 200 L230 356 L192 356 Z"
+            fill="none"
+            stroke="#1f2ea8"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="22"
         />
         <path
-            d="M320 144 L440 344 L404 344 L320 204 L236 344 L200 344 Z"
-            fill="#1f2ea8"
+            d="M320 235 L472 492 L434 492 L320 290 L206 492 L168 492 Z"
+            fill="none"
+            stroke="#1f2ea8"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="20"
         />
         <path
-            d="M320 244 L468 484 L430 484 L320 300 L210 484 L172 484 Z"
-            fill="#1f2ea8"
+            d="M166 538 L216 438 L248 438 L200 536 L200 648 L276 648 L320 520 L364 648 L440 648 L440 536 L392 438 L424 438 L474 538"
+            fill="none"
+            stroke="#1f2ea8"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="24"
         />
         <text
             x="320"
-            y="690"
+            y="720"
             text-anchor="middle"
             fill="#e31c24"
-            font-family="Arial, Helvetica, sans-serif"
-            font-size="120"
-            font-weight="700"
-            letter-spacing="8"
+            font-family="Arial Black, Arial, Helvetica, sans-serif"
+            font-size="112"
+            font-weight="900"
+            letter-spacing="6"
             style="paint-order: stroke fill"
             stroke="#1f2ea8"
-            stroke-width="14"
+            stroke-width="12"
             stroke-linejoin="round"
         >
             ASKA
